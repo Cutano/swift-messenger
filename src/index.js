7da727caacebf34a683c66760103f7a72ab6590e
Rinne <rinne@rinne.top>
2021-08-18 08:33:31 +0800

    Move reportWebVitals.js and serviceWorker.js to Utilities

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './Utilities/reportWebVitals';
+import * as serviceWorker from './Utilities/serviceWorker';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
